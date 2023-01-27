5435084a81482fb597cf28d71a6ee0341af6abcb
kalinin-ai <alexey.kalinin@exponenta.ru>
2023-01-27 20:32:35 +0300

    add epoch metric logging

--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,9 @@
 from torch.optim import Adam
 from pytorch_lightning import LightningModule
 from torch import nn
+from torchmetrics.functional import dice
+import torch 
+
 
 class SegmetationModule(LightningModule):
     def __init__(self, model, lr:float=1e-4, **model_args) -> None:
@@ -22,14 +25,33 @@ class SegmetationModule(LightningModule):
         preds = self.model(inputs)
         preds, masks = preds.squeeze(1), masks.squeeze(1)
         loss = self.criterion(preds, masks)
-        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        # self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
+    def training_epoch_end(self, outputs):
+        avg_loss = torch.stack(outputs).mean()
+        self.log('epoch_train_loss', avg_loss)
+
     def validation_step(self, batch, batch_idx):
-        # get validation loss
+        # get preds 
         inputs, masks = batch
         preds = self.model(inputs)
         preds, masks = preds.squeeze(1), masks.squeeze(1)
+        
+        # get validation loss
         loss = self.criterion(preds, masks)
-        self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
+
+        # get validation dice score 
+        dice_score = dice(preds, masks)
+
+        # self.log('val_dice', dice_score, on_step=False, on_epoch=False, logger=True, prog_bar=True)
+        # self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return {'loss' : loss, 'dice_score': dice_score}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
+        avg_dice = torch.stack([x['dice_score'] for x in outputs]).mean()
+
+        self.log('epoch_val_loss', avg_loss)
+        self.log('epoch_val_dice', avg_dice)
+        
